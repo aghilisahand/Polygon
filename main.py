8e861d72e53f8ca7d8478a6ee3f0e87e0addc9a9
aghilisahand <aghili.sahand@gmail.com>
2025-10-22 02:51:38 -0500

    got ticker list

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
-import os
+import os, time
 from dotenv import load_dotenv 
 from polygon import RESTClient
+from datetime import datetime
 
 # Load environment variables from .env
-load_dotenv()
+load_dotenv(override=True)
 
 # API init
-apiKey = os.environ.get('POLYGON_API_KEY')
+apiKey = os.environ.get('POLYGON_API_KEY1')
+print("API key loaded?", bool(apiKey))
 client = RESTClient(apiKey)
+print("Client created successfully!")
 
 ## Get a list of tickers
 
-ticker_type = "CS" # can change this to change what kind of stock you want, we default to common stock
-
 tickers = []
+print("Fetching tickers...")
+
+t0 = time.time()
 for t in client.list_tickers(
 	market="stocks",
 	active="true",
 	order="asc",
-	limit="5",
+	limit="1000",
 	sort="ticker",
 	):
+    print("Fetched:", t.ticker)
     tickers.append(t.ticker)
 
-print(tickers)
+
+print(len(tickers))
+dt = time.time() - t0
+print("time elapsed:", dt)
+
+# TODO: I've figured out how to get a list of active tickers, I now need a practical way of grabbing daily bar data for the past couple of years and save them
+# TODO: what is meant by 1 second 1 minute and 1 day bar data ? 
