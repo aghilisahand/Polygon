0f09392b4bb01cff24ca8e3ac6a76513b0e29a22
aghilisahand <aghili.sahand@gmail.com>
2025-10-30 16:12:12 -0500

    caveman method complete

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+# =========================
+# LIBRARIES
+# =========================
 import os, time
 from dotenv import load_dotenv 
 from polygon import RESTClient
-from datetime import datetime
+from datetime import date
+import pandas as pd  
+
+
+
+# =========================
+# CONFIG
+# =========================
 
 # Load environment variables from .env
 load_dotenv(override=True)
@@ -12,26 +22,107 @@ print("API key loaded?", bool(apiKey))
 client = RESTClient(apiKey)
 print("Client created successfully!")
 
-## Get a list of tickers
-
-tickers = []
-print("Fetching tickers...")
-
-t0 = time.time()
-for t in client.list_tickers(
-	market="stocks",
-	active="true",
-	order="asc",
-	limit="1000",
-	sort="ticker",
-	):
-    print("Fetched:", t.ticker)
-    tickers.append(t.ticker)
 
 
-print(len(tickers))
-dt = time.time() - t0
-print("time elapsed:", dt)
 
-# TODO: I've figured out how to get a list of active tickers, I now need a practical way of grabbing daily bar data for the past couple of years and save them
-# TODO: what is meant by 1 second 1 minute and 1 day bar data ? 
+
+# ==========================
+# CAVEMAN METHOD
+# ==========================
+
+def get_active_tickers():
+
+	"""
+	Fetch tickers for active stocks of that day
+	returns a list of tickers
+	"""
+        
+	tickers = []
+	print("Fetching tickers...")
+
+	for t in client.list_tickers(
+		market="stocks",
+		active="true",
+		order="asc",
+		limit="1000",
+		sort="ticker",
+		):
+		tickers.append(t.ticker)
+		
+	print("sample:", tickers[1:5])
+	return tickers
+
+
+def get_aggs(timespan, symbol, start_date="2025-01-20", end_date=None):
+
+	"""
+    Fetch aggregates for a symbol and return a pandas DataFrame.
+    No I/O here.
+    """
+	  
+	aggs = []
+
+	if end_date == None:
+		end_date = str(date.today())
+
+	# for t in tickers:
+	for a in client.list_aggs(
+			symbol,
+			1,
+			timespan,
+			start_date,
+			end_date,
+			adjusted="true",
+			sort="asc",
+			limit=10000,
+		): 
+			aggs.append(a)
+
+	print("sample:", aggs[1:2])
+
+	df = pd.DataFrame(aggs)
+	return df
+
+
+def save_aggs_to_csv(df, filename):
+    """
+    Save DataFrame to CSV
+    """
+    if df.empty:
+        print(f"No data to save for {filename}")
+        return
+    df.to_csv(filename, index=False)
+    print(f"Saved {filename}")
+
+def main():
+	get_active_tickers()
+	df = get_aggs("day", "AAPL")
+	save_aggs_to_csv(df,"AAPL_day_aggs.csv")
+
+if __name__ == "__main__":
+	main()
+
+
+# TODO: 1) implement error catching in my functions
+#	    2) create a plan for data gathering that can lead to a meaningful analysis and possibly one i can fit a model to ?
+
+
+
+import logging
+from pathlib import Path
+from typing import List, Tuple
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from polygon.exceptions import BadResponse
+
+
+
+# =========================
+# LOGGING
+# =========================
+
+# logging.basicConfig(
+#     level=logging.INFO,
+#     format="%(asctime)s | %(levelname)s | %(message)s",
+#     datefmt="%H:%M:%S",
+# )
+
